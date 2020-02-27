c967bef295412f72bc2f0046b629231f3caa5099
ishan gupta <ishangupta@ishans-mbp.home>
2020-02-26 22:24:27 -0500

    Add basic models (plug and play)

---
+++ lib/model.py
@@ -0,0 +1,21 @@
+from sklearn.ensemble import GradientBoostingClassifier
+from sklearn import svm
+
+class Model:
+
+    def __init__(self, x, y):
+        self.X_train = x
+        self.y_train = y
+
+    def gradient_boosting(self, **params):
+        self.reshape()
+        clf = GradientBoostingClassifier(params)
+        return clf.fit(X_train, y_train)
+
+    def svm_classification(self, **params):
+        self.reshape()
+        clf = svm.SVC(params)
+        return clf.fit(self.X_train, self.y_train)
+
+    def reshape(self):
+        self.X_train = self.X_train.reshape(len(self.X_train),-1)
