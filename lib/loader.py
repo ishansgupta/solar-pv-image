63d21da33d6c54dde757f9d7c7b7d374442aab5e
ishan gupta <ishangupta@ishans-mbp.home>
2020-02-26 22:58:50 -0500

    Cosmetic changes

--- lib/loader.py
+++ lib/loader.py
@@ -13,9 +13,9 @@ classification, and performance evaluation
 '''
 class Loader():
     def __init__(self, **kwargs):
-        self.dir_train_images  = './data/training/' if 'dir_train_images' not in kwargs else kwargs['dir_train_images']
-        self.dir_test_images   = './data/testing/' if 'dir_test_images' not in kwargs else kwargs['dir_test_images']
-        self.dir_train_labels  = './data/labels_training.csv' if 'dir_train_labels' not in kwargs else kwargs['dir_train_labels']
+        self.dir_train_images  = '../data/training/' if 'dir_train_images' not in kwargs else kwargs['dir_train_images']
+        self.dir_test_images   = '../data/testing/' if 'dir_test_images' not in kwargs else kwargs['dir_test_images']
+        self.dir_train_labels  = '../data/labels_training.csv' if 'dir_train_labels' not in kwargs else kwargs['dir_train_labels']
 
     def load_data(self):
         labels_pd = pd.read_csv(self.dir_train_labels)
